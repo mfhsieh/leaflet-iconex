823e9309d115bafa25e49c50b8396c92a92c4b3e
mfhsieh <mfhsieh@gmail.com>
2024-06-05 00:04:44 +0800

    v0.1

--- src/leaflet-iconex.js
+++ src/leaflet-iconex.js
@@ -38,24 +38,23 @@
 	<path stroke-width="1" d="M 16,0.5 C 7.4396,0.5 0.5,7.4396 0.5,16 C 0.5,19.9768 1.7958,23.3692 4.4470,26.3333 L 16,39.25 27.5530,26.3333 C 30.2042,23.3692 31.5,19.9768 31.5,16 31.5,7.4396 24.5604,0.5 16,0.5 Z" />
 </svg>`,
             iconSize: [32, 40],
-            iconAnchor: [16, 40],
-            popupAnchor: [0, -24],
+            iconAnchor: [16, 40],  // the coordinates of the "tip" of the icon (relative to its top left corner)
+            popupAnchor: [0, -24],  // the coordinates of the point from which popups will "open", relative to the icon anchor.
             iconFill: "#0d6efd",
             iconOpacity: 1,
             iconStroke: "#ffffff",
             iconStrokeOpacity: .5,
 
             htmlBack: `
-<svg width="32" height="40" viewBox="0 0 32 40" xmlns="http://www.w3.org/2000/svg">
-	<circle stroke-width="0" cx="16" cy="16" r="13" />
+<svg width="26" height="26" viewBox="-13 -13 26 26" xmlns="http://www.w3.org/2000/svg">
+	<circle stroke-width="0" r="13" />
 </svg>`,
-            backSize: [32, 40],
-            backAnchor: [0, 0],
+            backSize: [26, 26],
+            backAnchor: [16, 16],  // the center point of the background layer, relative to the upper left point of the icon
             backFill: "#ffffff",
             backOpacity: 1,
 
-            contentSize: null,
-            contentAnchor: [16, 16],
+            contentAnchor: [16, 16],  // the center point of the content layer, relative to the upper left point of the icon
         },
 
         initialize: function (options) {
@@ -75,17 +74,19 @@
             if (this.options.backAnchor) backStyles.push(`left: ${this.options.backAnchor[0]}px; top: ${this.options.backAnchor[1]}px`);
             if (this.options.backFill) backStyles.push(`fill: ${this.options.backFill}`);
             if (this.options.backOpacity) backStyles.push(`opacity: ${this.options.backOpacity}`);
+            backStyles.push(`transform: translate(-50%, -50%)`);
 
             const contentStyles = [];
             contentStyles.push(`position: absolute`);
             if (this.options.contentAnchor) contentStyles.push(`left: ${this.options.contentAnchor[0]}px; top: ${this.options.contentAnchor[1]}px`);
             contentStyles.push(`transform: translate(-50%, -50%)`);
+            contentStyles.push("display: flex");
+            contentStyles.push("align-items: center");
+            contentStyles.push("justify-content: center");
 
             const spanStyles = [];
-            spanStyles.push("display: flex");
-            spanStyles.push("align-items: center");
-            spanStyles.push("justify-content: center");
-            if (this.options.contentSize) spanStyles.push(`width: ${this.options.contentSize[0]}px; height: ${this.options.contentSize[1]}px`);
+            spanStyles.push("text-align: center");
+            spanStyles.push("line-height: 1");
 
             const divs = [];
             divs.push(`<div style="${iconStyles.join("; ")};">${this.options.htmlIcon}</div>`);
